566b01e4e986bce44aa81894985ac775ecdd5b22
Mina Gaid <mina-gaid@users.noreply.github.com>
2018-04-06 19:08:52 +0100

    First commit

---
+++ background.js
@@ -0,0 +1,13 @@
+var host = "https://hooktube.com";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://www.youtube.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
